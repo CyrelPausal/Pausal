08dfdbe62642ab7ba044da30ac1f97e620bf02cb
CyrelPausal <cyrelbern12345@gmail.com>
2024-03-14 17:57:04 +0800

    updated

--- js/app.js
+++ js/app.js
@@ -8,30 +8,30 @@ let Obj1 ={
     title: "THE MAN SONG",
     desc: " I would be complex, I would be coolThey'd say I played the field before I found someone to commit toAnd that would be okay for me to doEvery conquest I had made would make me more of a boss to you",
     btn:"Contact Me",
-    myImg: "../img/img1.png"
+    myImg: "../imgs/img1.png"
 }
 
 let b = document.getElementById("sectionTwo")
 
 let Obj2={
     title2: "Project Completed",
-    img1: "../img/img1.png",
+    img1: "../imgs/img1.png",
     desc1: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, est ratione aut veritatis placeat earum voluptatum blanditiis officiis soluta sunt quod atque modi dolor quibusdam, at illum commodi aspernatur quos.",
     btn1: "LearnMore",
-    img2: "../img/img1.png",
+    img2: "../imgs/img1.png",
     desc2: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, est ratione aut veritatis placeat earum voluptatum blanditiis officiis soluta sunt quod atque modi dolor quibusdam, at illum commodi aspernatur quos.",
     btn2: "Learn More",
-    img3: "../img/img1.png",
+    img3: "../imgs/img1.png",
     desc3: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magnam, est ratione aut veritatis placeat earum voluptatum blanditiis officiis soluta sunt quod atque modi dolor quibusdam, at illum commodi aspernatur quos.",
     btn3: "Learn More"
 }
 
-let {title, desc, btn, myImg} = Obj1
+const {title, desc, btn, myImg} = Obj1
 a.append(func2(title,desc,btn))
 a.append(func(myImg))
 
 
-let {title2, img1, desc1, btn1, img2, desc2, btn2, img3, desc3, btn3} = Obj2
+const {title2, img1, desc1, btn1, img2, desc2, btn2, img3, desc3, btn3} = Obj2
 b.append(func3(title2,img1,desc1,btn1,img2,desc2,btn2,img3,desc3,btn3)) 
 
 
